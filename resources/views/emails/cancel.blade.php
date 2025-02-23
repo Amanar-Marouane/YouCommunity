c2d502c318fbbb16b1788e55e50ea1e3e01211b9
Amanar Marouane <marouane.amanar07@gmail.com>
2025-02-23 14:55:07 +0000

    Modifu Email Form

--- resources/views/emails/cancel.blade.php
+++ resources/views/emails/cancel.blade.php
@@ -1,7 +1,7 @@
 <html>
 
 <body>
-    <h1>={{ $data['event_name'] }}'s event</h1>
+    <h1>{{ $data['event_name'] }}'s event</h1>
     <h3>Hello, {{ $data['name'] }}</h3>
     <p>{{ $data['message'] }}</p>
 </body>
